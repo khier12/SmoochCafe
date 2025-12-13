55e2ab86c244aa076af8091222cba4d64ccb69a8
Khier <syntaxx.error404@gmail.com>
2025-12-13 14:05:54 +0800

    update back to top

--- js/script.js
+++ js/script.js
@@ -387,3 +387,27 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+
+
+// =======================
+// BACK TO TOP BUTTON
+// =======================
+const backToTop = document.getElementById("backToTop");
+
+if (backToTop) {
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      backToTop.classList.add("show");
+    } else {
+      backToTop.classList.remove("show");
+    }
+  });
+
+  backToTop.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+}
